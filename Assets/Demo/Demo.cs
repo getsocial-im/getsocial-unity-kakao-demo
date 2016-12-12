f66d449e17867ce778dd46dddf859f2b221b6d5e
Taras Leskiv <leskiv.taras@gmail.com>
2016-12-12 14:16:30 +0100

    now logging referral data

--- Assets/Demo/Demo.cs
+++ Assets/Demo/Demo.cs
@@ -1,11 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
 using GetSocialSdk.Core;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Demo : MonoBehaviour
 {
+    private string _referralData = string.Empty;
+
+    public Text referralDataText;
+
     void Start()
     {
+        GetSocial.Instance.SetOnReferralDataReceivedListener(referralData =>
+            {
+                var referralDataToString = "\n";
+
+                foreach (Dictionary<string, string> dictionary in referralData)
+                {
+                    foreach (KeyValuePair<string, string> entry in dictionary)
+                    {
+                        referralDataToString += entry.Key + ": " + entry.Value + "\n";
+                    }
+
+                    referralDataToString += "---";
+                }
+               
+                var referralDataLog = "Referral data received: \n" + referralDataToString;
+                Debug.Log(referralDataLog);
+                _referralData = referralDataLog;
+            });
+        
         GetSocial.Instance.RegisterPlugin(KakaoInvitePlugin.ProviderName, new KakaoInvitePlugin());
 
         GetSocial.Instance.Init(() =>
@@ -16,6 +40,11 @@ public class Demo : MonoBehaviour
             {
                 Debug.Log("Failed to initialize GetSocial");
             });
+    }
+
+    void Update()
+    {
+        referralDataText.text = _referralData;
     }
 
     public void OnClick()
